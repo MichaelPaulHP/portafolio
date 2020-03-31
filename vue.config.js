8056b70b0ffbfa1bf376386cd1e7065d578cad3a
michael <michael_h_p_@hotmail.com>
2020-03-30 21:14:43 -0500

    this is a pwa

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,20 @@ module.exports = {
   "transpileDependencies": [
     "vuetify",
     "sass"
-  ]
+  ],
+  pwa: {
+    name: 'MichaelDev',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    }
+  }
 }
