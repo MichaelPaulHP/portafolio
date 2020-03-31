95264d4cd44a89b1359b46a78efc7d811430ff12
michael <michael_h_p_@hotmail.com>
2020-03-30 21:38:43 -0500

    added firebase analytics

--- src/main.js
+++ src/main.js
@@ -4,7 +4,28 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
 
-Vue.config.productionTip = false
+
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD72otQFhVhMt7uRBjMHxs9Hdz9_hjYOA",
+  authDomain: "michaelpauldev.firebaseapp.com",
+  databaseURL: "https://michaelpauldev.firebaseio.com",
+  projectId: "michaelpauldev",
+  storageBucket: "michaelpauldev.appspot.com",
+  messagingSenderId: "873393379457",
+  appId: "1:873393379457:web:9ce4800fd37ab70fe3d580",
+  measurementId: "G-VJF9JGJK00"
+};
+
+firebase.initializeApp(firebaseConfig);
+const analytics=firebase.analytics();
+analytics.logEvent("mainJS");
+
+Vue.config.productionTip = false;
+Vue.prototype.$analytics = analytics;
 
 new Vue({
   router,
